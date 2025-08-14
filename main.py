7698b937f8d098163089396620fa34d6daa3dc5d
Miłosz Kubicki <milosz.kubicki.97@gmail.com>
2025-08-14 03:10:03 +0200

    Added language selection function
    Fixed date-related errors when adding expenses

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ from datetime import datetime
 
 FILENAME = "expenses.json"
 
-def exception_handling(prompt,type_func, positive_only=False,is_date=False):
+def load_language():
+    input_language = exception_handling("Choose language (pl/en): ", str)
+    try:
+        with open(f"{input_language}.json", "r") as file:
+            language = json.load(file)
+            return language
+    except FileNotFoundError:
+        print("File with language not found")
+
+def exception_handling(prompt,type_func, positive_only=False,is_date=False, language=None):
     while True:
         user_input = input(prompt)
         try:
@@ -17,23 +26,24 @@ def exception_handling(prompt,type_func, positive_only=False,is_date=False):
 
                 if isinstance(value, (int,float)):
                     if positive_only and value<=0:
-                        print("Wartosci musza byc wieksze od zera")
+                        #print("Wartosci musza byc wieksze od zera")
+                        print(language['isinstance_number'])
                         continue
 
                 if isinstance(value, str):
                     value = value.strip()
                     if not value:
-                        print("Tekst nie moze byc pusty")
+                        print(language['isinstance_str'])
                         continue
 
             return value
 
         except ValueError:
             type_name= {
-                int:"Podaj liczbe calkowita",
-                float:"Podaj liczbe zmienno przecinkową",
-                str:"Wprowadz tekst",
-                'date': "Wprowadza poprawny format daty"
+                int:language['isistance_valueerror_int'],
+                float:language['isistance_valueerror_float'],
+                str:language['isistance_valueerror_str'],
+                'date': language['isinstance_valueerror_date']
             }.get('date' if is_date else type_func, str(type_func))
 
 def load_expenses():
@@ -58,14 +68,14 @@ def save_expenses(expenses):
     with open(FILENAME, "w") as file:
         json.dump(serializable_expenses, file, indent=4)
 
-def add_expense(expenses):
+def add_expense(expenses,language=None):
 
-    item_name = exception_handling("Wprowadz nazwe zakupionego produktu ", str)
-    quantity = exception_handling("Wprowadz ilosc zakupionego produktu ", int,positive_only=True)
-    value = exception_handling("Wprowadz wartość produktu ", float,positive_only=True)
-    date = exception_handling("Wprowadz date zakupu ", str,is_date=True)
-    date_obj = datetime.strptime(date, "%d-%m-%Y").date()
-    category = exception_handling("Wprowadz kategorie produktu ", str)
+    item_name = exception_handling(language['item_name'], str)
+    quantity = exception_handling(language['quantity'], int,positive_only=True)
+    value = exception_handling(language['value'], float,positive_only=True)
+    date = exception_handling(language['date'], str,is_date=True)
+    #date_obj = datetime.strptime(date, "%d-%m-%Y").date()
+    category = exception_handling(language['category'], str)
 
 
 
@@ -73,43 +83,43 @@ def add_expense(expenses):
         "item_name": item_name,
         "quantity": quantity,
         "value": value,
-        "date": date_obj,
+        "date": date,
         "category": category
     })
     save_expenses(expenses)
-    print("Expenses saved")
+    print(language['expenses_saved'])
 
-def del_expense(expenses):
-    input_del = exception_handling("Wprowadz liczbe wydatku do usuniecia",int,positive_only=True) - 1
+def del_expense(expenses,language=None):
+    input_del = exception_handling(language['input_del'],int,positive_only=True) - 1
     del expenses[input_del]
 
-def edit_expense(expenses):
-    input_record = exception_handling("Wybierz ktory wydatek chcesz edytowac",int,positive_only=True) - 1
+def edit_expense(expenses,language=None):
+    input_record = exception_handling(language['input_record'],int,positive_only=True) - 1
     while True:
-        print("\n=== Wybor wpisu do edycji ===")
+        print(f"\n{language['menu_edit']}")
         print(f"1. {expenses[input_record]['item_name']}")
         print(f"2. {expenses[input_record]['quantity']}")
         print(f"3. {expenses[input_record]['value']}")
         print(f"4. {expenses[input_record]['date']}")
         print(f"5. {expenses[input_record]['category']}")
-        print(f"6. Exit")
+        print(f"6. {language['menu_exit']}")
 
-        input_edit_record = exception_handling("Wybierz ktora czesc chcesz edytowac",int,positive_only=True)
+        input_edit_record = exception_handling(language['input_edit_record'],int,positive_only=True)
 
         if input_edit_record == 1:
-            expenses[input_record]['item_name'] = exception_handling("Wprowadz nowa wartosc",str)
+            expenses[input_record]['item_name'] = exception_handling(language['input_edit_record_1'],str)
         elif input_edit_record == 2:
-            expenses[input_record]['quantity'] = exception_handling("Wprowadz nowa wartosc",int,positive_only=True)
+            expenses[input_record]['quantity'] = exception_handling(language['input_edit_record_2'],int,positive_only=True)
         elif input_edit_record == 3:
-            expenses[input_record]['value'] = exception_handling("Wprowadz nowa wartosc", float,positive_only=True)
+            expenses[input_record]['value'] = exception_handling(language['input_edit_record_3'], float,positive_only=True)
         elif input_edit_record == 4:
-            expenses[input_record]['date'] = exception_handling("Wprowadz nowa wartosc DD-MM-YYYY",str,is_date=True)
+            expenses[input_record]['date'] = exception_handling(language['input_edit_record_4'],str,is_date=True)
         elif input_edit_record == 5:
-            expenses[input_record]['category'] = exception_handling("Wprowadz nowa wartosc",str)
+            expenses[input_record]['category'] = exception_handling(language['input_edit_record_5'],str)
         else:
             break
 
-def view_expenses(expenses):
+def view_expenses(expenses,language=None):
 
     display_list = []
     for idx,expense in enumerate(expenses, start=1):
@@ -120,7 +130,7 @@ def view_expenses(expenses):
     choose_list = display_list
 
 
-    prompt_view_sort = "1. Sortowanie po dacie\ndowolna inna liczba aby kontynuowac bez sortowania\n"
+    prompt_view_sort = f"{language['prompt_view_sort']}\n"
     input_sort = exception_handling(prompt_view_sort, int, positive_only=True, is_date=False)
     if input_sort == 1:
         display_list_sorted = sorted(display_list, key=lambda display_list: display_list['date'])
@@ -132,9 +142,9 @@ def view_expenses(expenses):
 
 
 
-def show_summary(expenses):
+def show_summary(expenses,language=None):
     total = sum(expense['value'] for expense in expenses)
-    print(f"\nTotal spent: PLN{total: .2f}")
+    print(f"\n{language['show_summary_total']}{total: .2f}")
 
 def category_list(expenses):
     seen_category = set()
@@ -152,51 +162,51 @@ def category_list(expenses):
         print(f"{expense['id']}. {expense['category']}")
     return list_category
 
-def cateogry_summary(expenses):
+def cateogry_summary(expenses,language=None):
     list_category = category_list(expenses)
     total_category = 0
-    input_category = exception_handling("\nWybierz jedna z kategorii",int) - 1
+    input_category = exception_handling(f"\n{language['input_category']}",int) - 1
 
     for expense in expenses:
         if expense['category'] == list_category[input_category]['category']:
             total_category += expense['value']
-    print(f"\nTotal spent: PLN{total_category: .2f}")
+    print(f"\n{language['show_summary_total']}{total_category: .2f}")
 
 def main():
     expenses = load_expenses()
-
+    language = load_language()
     while True:
-        print("\n=== Personal Expense Tracker ===")
-        print("1. Add Expense")
-        print("2. View Expenses")
-        print("3. Show Summary")
-        print("4. Show Summary from category")
-        print("5. Delete Expense")
-        print("6. Edit Expense")
-        print("7. Exit")
+        print(f"\n{language['main_menu_title']}")
+        print(f"1. {language['main_menu_1']}")
+        print(f"2. {language['main_menu_2']}")
+        print(f"3. {language['main_menu_3']}")
+        print(f"4. {language['main_menu_4']}")
+        print(f"5. {language['main_menu_5']}")
+        print(f"6. {language['main_menu_6']}")
+        print(f"7. {language['menu_exit']}")
 
-        choice = exception_handling("\nWybierz akcje menu\n",int, positive_only=True)
+        choice = exception_handling(f"\n{language['main_menu_choice']}\n",int, positive_only=True)
 
         if choice == 1:
-            add_expense(expenses)
+            add_expense(expenses,language)
         elif choice == 2:
-            view_expenses(expenses)
+            view_expenses(expenses,language)
         elif choice == 3:
-            show_summary(expenses)
+            show_summary(expenses,language)
         elif choice == 4:
-            cateogry_summary(expenses)
+            cateogry_summary(expenses,language)
         elif choice == 5:
-            view_expenses(expenses)
-            del_expense(expenses)
-            save_expenses(expenses)
+            view_expenses(expenses,language)
+            del_expense(expenses,language)
+            save_expenses(expenses,language)
         elif choice == 6:
-            view_expenses(expenses)
-            edit_expense(expenses)
-            save_expenses(expenses)
+            view_expenses(expenses,language)
+            edit_expense(expenses,language)
+            save_expenses(expenses,language)
         elif choice == 7:
             break
         else:
-            print("Invalid input")
+            print(language['invalid_input'])
 
 if __name__ == "__main__":
     main()
